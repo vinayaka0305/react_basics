69d50a43dfa15c4631a9677ffc31cd26d1e82ade
vinayaka0305 <vav6473@gmail.com>
2024-08-01 17:08:24 +0530

    code

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import Theme_pro from "./theme_context_p/Theme_pro";
 // import LightDark from "./component/light_dark/LightDark";
 // import Counter from "./component/use_reducer/Counter";
 // import UseRef from "./component/useRef/UseRef";
 // import UseRef2 from "./component/useRef/UseRef2";
-import UseMemo from "./component/useMemo/UseMemo";
+// import UseMemo from "./component/useMemo/UseMemo";
+// import NewTheme from "./component/NewTheme";
+// import UseCallBack from "./component/useCallback/UseCallBack";
+import Example from "./component/customHook/Example";
+
 
 const App = () => {
   return (
     <>
-      <Theme_pro>
-        <UseMemo/>
-      </Theme_pro>
+      <Example/>
     </>
   );
 };
